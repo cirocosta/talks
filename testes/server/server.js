2220dc615d48ebf22b1a5e3fab5b6c419c26bed1
Ciro S. Costa <ciro.costa@usp.br>
2014-08-28 02:35:18 -0300

    sets basic socketio

--- testes/server/server.js
+++ testes/server/server.js
@@ -1,9 +1,24 @@
 'use strict';
 var express = require('express')
+  , app = express()
+  , http = require('http').Server(app)
+  , io = require('socket.io')(http)
   , logger = require('morgan');
 
-var app = express();
 
 app.use(logger('combined'));
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  socket.emit('news', {hello: 'world'});
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
+http.listen(3000, function () {
+  console.log('Listening on *:3000');
+});
